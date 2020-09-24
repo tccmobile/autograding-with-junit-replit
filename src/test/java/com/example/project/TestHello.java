f08beba04a8609153ce846830b5fd351e06aa905
tccmobile <William.smith@tulsacc.edu>
2020-09-24 15:16:44 -0500

    Initial commit

---
+++ src/test/java/com/example/project/TestHello.java
@@ -0,0 +1,33 @@
+package com.example.project;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+
+import java.io.*;
+
+public class TestHello {
+
+   public static void main(String args[]) {
+      testHelloWorld();
+   }
+   @Test
+   public static void testHelloWorld()
+   {
+     PrintStream originalOut = System.out;
+     ByteArrayOutputStream bos = new ByteArrayOutputStream();
+     System.setOut(new PrintStream(bos));
+
+     // action
+     Hello.main(null);
+
+     // assertion
+     assertEquals("Hello world!\n", bos.toString());
+
+     // undo the binding in System
+     System.setOut(originalOut);
+   }
+}
